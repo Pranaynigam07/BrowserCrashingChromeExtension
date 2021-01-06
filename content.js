8c91ef76dacafde38fc2a6d0e59decad824c07f4
Pranaynigam07 <32392829+Pranaynigam07@users.noreply.github.com>
2021-01-06 18:25:44 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,7 @@
+window.addEventListener("mouseover",mouseEventHandler);
+
+
+ function mouseEventHandler(e){
+   alert("Your System is hanged!!!!!!!!!!!!!!!!!!! \n Dont Shake the mouse \n or else\n mouse will Shake you out")
+
+ }
